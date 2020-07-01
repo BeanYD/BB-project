7055149b6d762869d80226be23f2e2cc8685e87a
dingbinbin <ding16goooo@gmail.com>
2020-07-01 13:55:05 +0800

    update

--- BB-project/iOS基础/性能优化/Controller/BBPerformOptiStartViewController.m
+++ BB-project/iOS基础/性能优化/Controller/BBPerformOptiStartViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "BBPerformOptiStartViewController.h"
+#import <WebKit/WebKit.h>
 
 @interface BBPerformOptiStartViewController ()
+
+@property (strong, nonatomic) WKWebView *webView;
 
 @end
 
@@ -17,6 +20,33 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    [self.view addSubview:self.webView];
+    
+    [self layoutSubviews];
+    
+    NSString * path = [[NSBundle mainBundle] pathForResource:@"performOptiStart" ofType:@"html"];
+    // 创建URL
+    NSURL * url = [NSURL fileURLWithPath:path];
+    // 创建NSURLRequest
+    NSURLRequest * request = [NSURLRequest requestWithURL:url];
+    // 加载
+    [self.webView loadRequest:request];
+}
+
+- (void)layoutSubviews {
+    [self.webView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.edges.equalTo(self.view);
+    }];
+}
+
+#pragma mark - getter and setter
+- (WKWebView *)webView {
+    if (!_webView) {
+        _webView = [[WKWebView alloc] init];
+    }
+    
+    return _webView;
 }
 
 @end
