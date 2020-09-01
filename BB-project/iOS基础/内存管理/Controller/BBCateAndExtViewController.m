170f9d1279354342cf209240465ab3e33464d74b
B.Ding <742819176@qq.com>
2020-09-02 07:04:13 +0800

    update

--- BB-project/iOS基础/内存管理/Controller/BBCateAndExtViewController.m
+++ BB-project/iOS基础/内存管理/Controller/BBCateAndExtViewController.m
@@ -20,7 +20,15 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
-    TestForCategory *test = [[TestForCategory alloc] init];
+    /**
+     1.创建一个单例收集当前的nslog，并保存，界面展示时，进行读取
+     2.针对需要进行代码验证的一些内容
+     */
+    
+    // 验证分类执行的顺序已经执行分类本类的同名方法
+    [[TestForCategory alloc] init];
+    
+    
 }
 
 
